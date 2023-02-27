613271a46bbacc0abbe6b248ae88ec523fd4995b
Vespertino <Vespertino@C04PC19.informatica.comercio>
2023-02-27 17:21:10 +0100

    Codigo Base

---
+++ Practica_UD3_UD4/src/JavierPascual_Pintar.java
@@ -0,0 +1,14 @@
+
+public class JavierPascual_Pintar {
+	public String nombreCuadro;
+	public int nColores;
+	public String nombreHobby;
+	
+	public JavierPascual_Pintar(String nombreCuadro, int nColores, String nombreHobby) {
+		super();
+		this.nombreCuadro = nombreCuadro;
+		this.nColores = nColores;
+		this.nombreHobby = nombreHobby;
+	}
+}
+
