ac71862f231d26985cf64671484a4c84f8ad63ac
Vespertino <Vespertino@C04PC19.informatica.comercio>
2023-02-27 17:33:26 +0100

    Primer getter

--- Practica_UD3_UD4/src/JavierPascual_Pintar.java
+++ Practica_UD3_UD4/src/JavierPascual_Pintar.java
@@ -3,12 +3,68 @@ public class JavierPascual_Pintar {
 	public String nombreCuadro;
 	public int nColores;
 	public String nombreHobby;
+	public String nombrePintor;
+	public int medidasCuadro;
+	public int precioCuadro;
 	
-	public JavierPascual_Pintar(String nombreCuadro, int nColores, String nombreHobby) {
+	public JavierPascual_Pintar(String nombreCuadro, int nColores, String nombreHobby, String nombrePintor, int medidasCuadro, int precioCuadro) {
 		super();
 		this.nombreCuadro = nombreCuadro;
 		this.nColores = nColores;
 		this.nombreHobby = nombreHobby;
+		this.nombrePintor = nombrePintor;
+		this.medidasCuadro = medidasCuadro;
+		this.precioCuadro = precioCuadro;
 	}
+
+	public String getNombreCuadro() {
+		return nombreCuadro;
+	}
+
+	public void setNombreCuadro(String nombreCuadro) {
+		this.nombreCuadro = nombreCuadro;
+	}
+
+	public int getnColores() {
+		return nColores;
+	}
+
+	public void setnColores(int nColores) {
+		this.nColores = nColores;
+	}
+
+	public String getNombreHobby() {
+		return nombreHobby;
+	}
+
+	public void setNombreHobby(String nombreHobby) {
+		this.nombreHobby = nombreHobby;
+	}
+
+	public String getNombrePintor() {
+		return nombrePintor;
+	}
+
+	public void setNombrePintor(String nombrePintor) {
+		this.nombrePintor = nombrePintor;
+	}
+
+	public int getMedidasCuadro() {
+		return medidasCuadro;
+	}
+
+	public void setMedidasCuadro(int medidasCuadro) {
+		this.medidasCuadro = medidasCuadro;
+	}
+
+	public int getPrecioCuadro() {
+		return precioCuadro;
+	}
+
+	public void setPrecioCuadro(int precioCuadro) {
+		this.precioCuadro = precioCuadro;
+	}
+	
+	
 }
 
